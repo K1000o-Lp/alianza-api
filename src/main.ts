9d283843044af1e1441e4c3f6385b039cabf60a6
k1000o-lp <camiloalfonsolopezparra@gmail.com>
2024-06-21 13:53:35 -0400

    feat(route): add global prefix 'api' feat(cors): set global cors to all origin

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cookieParser, * as CookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  app.setGlobalPrefix('api');
+
+  app.enableCors();
 
 
   await app.listen(3000);
