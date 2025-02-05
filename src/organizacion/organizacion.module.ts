4403c8e4a8a6850f16bc453a512aba99c18cc6a4
k1000o-lp <camiloalfonsolopezparra@gmail.com>
2025-02-05 14:23:03 -0400

    feat(organizacion): add ZonaSupervisor entity to OrganizacionModule imports

--- src/organizacion/organizacion.module.ts
+++ src/organizacion/organizacion.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrganizacionController } from './organizacion.controller';
 import { OrganizacionService } from './organizacion.service';
-import { HistorialMiembro, Servicio, Zona } from './entities';
+import { HistorialMiembro, Servicio, Zona, ZonaSupervisor } from './entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Zona, Servicio, HistorialMiembro])],
+  imports: [TypeOrmModule.forFeature([Zona, Servicio, HistorialMiembro, ZonaSupervisor])],
   controllers: [OrganizacionController],
   providers: [OrganizacionService],
   exports: [OrganizacionService],
